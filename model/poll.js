5883f6683ff33cb9442f265ed0342128fa98b508
Jeffrey Gebhardt <jeff@gebbey.com>
2016-08-04 12:45:09 -0700

    added poll schema

---
+++ model/poll.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const PollSchema = new mongoose.Schema({
+  pollName: { type: String, required: true, unique: true },
+  createdAt: { type: Date, default: Date.now },
+  pollStatus: {
+    type: String,
+    required: true,
+    enum: 'not_started in_progress completed'
+    .split(' ') },
+  choices: [{
+    type: String,
+    required: true,
+  }],
+  votes: [{
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    vote: {
+      type: String,
+      required: true,
+    },
+  }],
+});
+
+module.exports = exports = mongoose.model('Poll', PollSchema);
