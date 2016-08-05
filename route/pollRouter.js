b3468fcbf97eb75d084cf5169afcfd69f126eb49
Jeffrey Gebhardt <jeff@gebbey.com>
2016-08-04 21:15:03 -0700

    added poll routes

---
+++ route/pollRouter.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const jsonParser = require('body-parser').json();
+const express = require('express');
+
+const Poll = require('../model/poll');
+
+const pollRouter = module.exports = exports = express.Router();
+
+
+pollRouter.post('/', jsonParser, (req, res, next) => {
+  const newPoll = new Poll(req.body);
+  newPoll.save((err, poll) => {
+    if (err) return next(err);
+    return res.json(poll);
+  });
+});
+
+pollRouter.get('/:id', (req, res, next) => {
+  const _id = req.params.id;
+  Poll.findOne({_id}, (err, poll) => {
+    if (err) return next(err);
+    return res.json(poll);
+  });
+});
+
+pollRouter.get('/', (req, res, next) => {
+  Poll.find({}, (err, polls) => {
+    if (err) return next(err);
+    return res.json(polls);
+  });
+});
+
+pollRouter.get('/sms_callback', (req, res, next) => {
+  console.log(req.query);
+  res.status(200).end();
+  next();
+});
