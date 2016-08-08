8780749db26aa67eefba6e6e6a272d34c88eeca5
Mars <marshallmiller92@gmail.com>
2016-08-08 13:59:00 -0700

    finally working, atom bug

--- test/model/routerTests.js
+++ test/model/routerTests.js
@@ -8,17 +8,27 @@ chai.use(chaiHttp);
 
 const expect = chai.expect;
 const request = chai.request;
-
-// const Poll = require('../../model/poll');
-
-const port = process.env.PORT || 3141;
-
-mongoose.connect('mongodb://localhost/votes-real-test');
+process.env.MONGODB_URI = 'mongodb://localhost/votes-real-test';
+process.env.PASSWORD = 'testpass';
+const server = require('../../lib/server');
+const Poll = require('../../model/poll');
+const testHarness = require('./testHarness');
 
 describe('CRUD testing', () => {
+  before(function(done) {
+    this.newPoll = new Poll({ pollName: 'test poll' });
+    done();
+  });
+
+  after((done) => {
+    Poll.remove({});
+    done();
+  });
+
   it('should post a poll with a name and choices', (done) => {
-    request(port)
+    request(server)
     .post('/api/poll')
+    .auth('admin', 'testpass')
     .send({
       pollName: 'test poll',
       choices: [
@@ -31,7 +41,18 @@ describe('CRUD testing', () => {
       expect(err).to.eql(null);
       expect(res).to.have.status(200);
       expect(res.body.pollName).to.eql('test poll');
-      expect(res.body.choices).to.eql();
+      // expect(res.body.choices).to.eql();
+      done();
+    });
+  });
+
+  it('should get a poll', function(done) {
+    request(server)
+    .get('/api/poll')
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.body[0].pollName).to.eql(this.newPoll.pollName);
       done();
     });
   });
@@ -50,19 +71,4 @@ describe('CRUD testing', () => {
   // });
   //
   //
-  // it('should get a vote', (done) => {
-  //   request('localhost:3141')
-  //   .get('/api')
-  //
-  //   .end((err, res) => {
-  //     expect(err).to.eql(null);
-  //     expect(res).to.have.status(200);
-  //     //other tests?
-  //     done();
-  //   })
-  // });
-  //
-  // it('should update test', (done) => {
-  //
-  // });
 });
