95f1e8a9a757fc10d7a5cfe80d7f31fdb9e4656b
Gio d'Amelio <giodamelio@gmail.com>
2016-08-08 11:35:22 -0700

    Convert admin cli to vorpal

--- bin/adminTools.js
+++ bin/adminTools.js
@@ -3,60 +3,48 @@
 
 const program = require('inquirer');
 const request = require('request');
+const vorpal = require('vorpal');
 
 const addPollBaseUrl = 'https://real-votes.herokuapp.com/api/poll';
 const addPollBaseUrlTest = 'http://localhost:3141/api/poll';
 
 console.log('Hello welcome to the real-votes admin console.');
 
-const adminOptionsPrompt = {
-  type: 'list',
-  name: 'requestType',
-  message: 'What would you like to do?',
-  choices: ['addPoll', 'updatePollStatus', 'exit'],
-};
+const cli = vorpal();
 
-const addPollQuestions = [
-  {
-    type: 'input',
-    name: 'pollName',
-    message: 'What would you like to name your poll?',
-  },
-];
-
-function addPoll() {
-  program.prompt(addPollQuestions).then((answers) => {
-    JSON.stringify(answers, null, '  ');
-
-    const options = {
-      url: addPollBaseUrlTest,
-      json: { pollName: answers.pollName },
-      auth: {
-        username: 'admin',
-        password: process.env.PASSWORD,
+cli
+  .command('addPoll', 'Creates a new poll')
+  .action(function(args, callback) {
+    this.prompt([
+      {
+        type: 'input',
+        name: 'pollName',
+        message: 'What would you like to name your poll? ',
       },
-    };
-    console.log(options);
-    request.post(options, (err) => {
-      if (err) return console.log(err);
-      return console.log('Success!');
+    ], (answers) => {
+      const options = {
+        url: addPollBaseUrlTest,
+        json: { pollName: answers.pollName },
+        auth: {
+          username: 'admin',
+          password: process.env.PASSWORD,
+        },
+      };
+
+      request.post(options, (err) => {
+        if (err) return console.log(err);
+        this.log('Success!');
+        callback();
+      });
     });
   });
-}
 
-function chooseOption() {
-  program.prompt(adminOptionsPrompt).then((answers) => {
-    if (answers.requestType === 'addPoll') {
-      console.log('You choose %s ', answers.requestType);
-      addPoll();
-    }
-    if (answers.requestType === 'updatePollStatus') {
-      console.log('You choose %s ', answers.requestType);
-    }
-    if (answers.requestType === 'exit') {
-      process.exit();
-    }
+cli
+  .command('updatePollStatus', 'Updates the status of a poll')
+  .action(function(args, callback) {
+    callback();
   });
-}
 
-chooseOption();
+cli
+  .delimiter('real-votes$ ')
+  .show();
