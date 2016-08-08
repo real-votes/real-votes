562f4f0bc7edc34c167da5e5f110476cb170d3a2
Mars <marshallmiller92@gmail.com>
2016-08-08 12:33:41 -0700

    server actually works now

--- test/model/testServer.js
+++ test/model/testServer.js
@@ -1,6 +1,7 @@
 const server = require('../../lib/server');
 
-const port = process.env.PORT || 3141;
+const port = process.env.PORT || 3000;
+
 server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
