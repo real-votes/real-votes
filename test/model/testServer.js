50affd6c7061590f7f4c4edb642565c3af423f50
Gio d'Amelio <giodamelio@gmail.com>
2016-08-15 10:31:49 -0700

    Get rid of old test files

--- test/model/testServer.js
+++
@@ -1,7 +0,0 @@
-const server = require('../../lib/server');
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
